2eeae838cc6996046bb1b2941b1011f077050822
S-acharya57 <acharyasa38@gmail.com>
2024-04-09 12:49:07 +0545

    Multiplication and Division Function Added
    Two basic functions for product and division are added. Further comments
    are missing in the code.

--- simplify.py
+++ simplify.py
@@ -4,6 +4,14 @@ def add(x, y):
 def subtract(x, y):
     return x-y 
 
+def mult(x, y):
+    return x*y
+
+def divide(x, y):
+    return x//y
+
+
 print(add(3,4))
 print(subtract(4,3))
-
+print(mult(3,4))
+print(divide(12,5))
